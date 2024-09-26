c00b2b151c83f850d13ff81a48852bdc61e92824
Vijay Kumar <munukotivjiaykumar@gmail.com>
2024-09-26 04:43:54 -0400

    add cart view controller  and fixed the issues from navigation bar item update

---
+++ MVFoods/View/Cart/CartTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CartTableViewCell.swift
+//  MVFoods
+//
+//  Created by Vijay Kumar Munukoti on 9/26/24.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
